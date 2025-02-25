c6bbc15e5d1ce22d1cc05b1c284d6243a2efde30
Kristian <kristian.ivanov@extreme.consulting>
2025-02-25 13:03:15 +0200

    Changed logo.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,13 +17,14 @@ export const Logo = (props: Props) => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      width={193}
+      width={103}
       height={34}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      src="./images.png"
+      // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
     />
   )
 }
