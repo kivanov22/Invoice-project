eb40252e77fef876bfe4547a7818e12020400041
Kristian <kristian.ivanov@extreme.consulting>
2025-02-26 16:39:04 +0200

    Added language switch option.

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -7,12 +7,27 @@ import type { Header as HeaderType } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
+import Switch from '@mui/material/Switch'
+import { useTranslation } from 'react-i18next'
+import { Typography } from '@mui/material'
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
+  const [checked, setChecked] = React.useState(true)
+  const { i18n } = useTranslation()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      i18n.changeLanguage('en')
+      setChecked(event.target.checked)
+    } else {
+      i18n.changeLanguage('bg')
+      setChecked(false)
+    }
+  }
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex gap-3 items-center h-30">
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="link" />
       })}
@@ -20,6 +35,13 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
         <span className="sr-only">Search</span>
         <SearchIcon className="w-5 text-primary" />
       </Link>
+      <Typography>EN</Typography>
+      <Switch
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
+      <Typography>BG</Typography>
     </nav>
   )
 }
