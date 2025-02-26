05ba232b7c1293dbb4f0d69ea4cb4f92faf3ae42
Kristian <kristian.ivanov@extreme.consulting>
2025-02-26 16:45:53 +0200

    Created sub footer.

--- src/components/SubFooter/index.tsx
+++ src/components/SubFooter/index.tsx
@@ -4,7 +4,7 @@ import { Logo } from '../Logo/Logo'
 const SubFooter: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-20 bg-gray-200 dark:bg-gray-800 gap-5">
-      <Logo loading="eager" priority="high" className="w-20" />
+      <Logo loading="eager" priority="high" className="" />
       <h1 className="text-3xl text-black dark:text-white">Extreme Consulting</h1>
     </div>
   )
